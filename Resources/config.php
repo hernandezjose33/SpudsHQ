c643e219c03f5cb61d80df1ac97c6053e79aa9bf
hernandezjose33 <karakuri333@gmail.com>
2020-10-06 17:02:46 -0500

    added paths to front and back folders

--- Resources/config.php
+++ Resources/config.php
@@ -2,9 +2,20 @@
     //If defined then make null
     defined("DS") ? null : define("DS", DIRECTORY_SEPARATOR);
 
-    //give the directory
-    echo __DIR__;
+    defined("TEMPLATE_FRONT") ? null : define("TEMPLATE_FRONT", __DIR__ . DS . "templates/front");
+    defined("TEMPLATE_BACK") ? null : define("TEMPLATE_BACK", __DIR__ . DS . "templates/back");
 
     //gives the directory all the way to the file
     //echo __FILE__;
+    
+    //give the directory
+    echo TEMPLATE_BACK;
+
+
+
+
+
+
+
+
 ?>
